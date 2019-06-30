3026c162fc97d09721315c8a83e274cbafaede97
mxito3 <1477311729@qq.com>
2019-06-30 21:45:47 +0800

    add gunicorn log

--- mysite/gunicorn.conf.py
+++ mysite/gunicorn.conf.py
@@ -3,7 +3,11 @@ import multiprocessing
 
 bind = "0.0.0.0:8000"   #绑定的ip与端口
 workers = 2                #核心数
-#errorlog = '/home/xxx/xxx/gunicorn.error.log' #发生错误时log的路径
-#accesslog = '/home/xxx/xxx/gunicorn.access.log' #正常时的log路径
-#loglevel = 'debug'   #日志等级
-proc_name = 'mysite'   #进程名
+# errorlog = 'gunicorn.error.log' #发生错误时log的路径
+accesslog = 'gunicorn.access.log' #正常时的log路径
+loglevel = 'debug'   #日志等级
+proc_name = 'mysite'   #进程名 
+
+
+
+
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'mysite.middleware.StatsMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
@@ -105,7 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai' 
+
+# TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
@@ -118,3 +121,78 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#日志
+LOG_PATH = os.path.join(BASE_DIR, "log/")
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s '
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
+        'file':{
+            'level':'INFO',
+            'class':'logging.handlers.RotatingFileHandler',
+            'formatter':'verbose',
+            'filename':LOG_PATH+"request.log",
+        },
+    },
+    'loggers': {
+        # 'django': {   #详细的
+        #     'handlers': ['console'],
+        #     'propagate': False,
+        # },
+        'django.request': {   #只显示404和500
+        'handlers': ['console'],
+        'propagate': False,
+        'level': 'DEBUG',
+        },
+        'django.server': {   #具体请求的url
+            'handlers': ['file','console'],
+            'propagate':False,
+            'level': 'DEBUG',
+        },
+        'django.db.backends': {  #数据库
+            'handlers': ['console','file'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
+
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': True,
+#     'formatters': {
+#         'verbose': {
+#             'format': '%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(module)s %(process)d %(thread)d %(message)s'
+#         }
+#     },
+#     'handlers': {
+#         'gunicorn': {
+#             'level': 'DEBUG',
+#             'class': 'logging.handlers.RotatingFileHandler',
+#             'formatter': 'verbose',
+#             'filename': 'gunicorn.errors',
+#             'maxBytes': 1024 * 1024 * 100,  # 100 mb
+#         }
+#     },
+#     'loggers': {
+#         'gunicorn.errors': {
+#             'level': 'DEBUG',
+#             'handlers': ['gunicorn'],
+#             'propagate': True,
+#         },
+#     }
+# }
